9afdd78c4c790bfdf123539914ba746907c30825
Cristian Guardado <crissge2016@gmail.com>
2022-09-05 15:08:00 -0600

    Icono y cambio de key

--- app/src/main/java/com/example/mituri/Modelo/ServiceAPI.java
+++ app/src/main/java/com/example/mituri/Modelo/ServiceAPI.java
@@ -8,10 +8,10 @@ import retrofit2.http.Path;
 
 public interface ServiceAPI {
 
-    @GET("all/?key=8e07aa7a15864d27093aed6b174b977f")
+    @GET("all/?key=b4fec2c5ace515d11943eab9ffeb12cb")
     Call<List<ModPaises>> getDatos();
 
-    @GET("{CodePais}/all/?key=8e07aa7a15864d27093aed6b174b977f")
+    @GET("{CodePais}/all/?key=b4fec2c5ace515d11943eab9ffeb12cb")
     Call<List<ModRegiones>> find(@Path("CodePais") String CodePais);
 
 }
