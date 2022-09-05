9fc0578f685b7832e78a7fb4786917c6a77b0a25
UlisesCH <ulisescoreas90@gmail.com>
2022-09-05 12:18:46 -0600

    Editar Sitio Turistico

--- app/src/main/java/com/example/mituri/Modelo/ServiceAPI.java
+++ app/src/main/java/com/example/mituri/Modelo/ServiceAPI.java
@@ -8,10 +8,10 @@ import retrofit2.http.Path;
 
 public interface ServiceAPI {
 
-    @GET("all/?key=8e07aa7a15864d27093aed6b174b977f")
+    @GET("all/?key=93fb10d40352cfcd2ca2d831a1f4d264")
     Call<List<ModPaises>> getDatos();
 
-    @GET("{CodePais}/all/?key=8e07aa7a15864d27093aed6b174b977f")
+    @GET("{CodePais}/all/?key=93fb10d40352cfcd2ca2d831a1f4d264")
     Call<List<ModRegiones>> find(@Path("CodePais") String CodePais);
 
 }
